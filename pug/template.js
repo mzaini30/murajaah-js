4b88f36a17e78361bfc14d67ee82d2e2010b88ee
mzaini30 <muhzaini30@gmail.com>
2020-04-10 05:40:58 +0800

    upload

--- pug/template.js
+++ pug/template.js
@@ -5,3 +5,12 @@ $('.navbar-brand').click(() => {
 		scrollTop: 0
 	}, 800)
 })
+
+if ('serviceWorker' in navigator) {
+	console.log("Will the service worker register?");
+	navigator.serviceWorker.register('service-worker.js').then(function(reg){
+		console.log("Yes, it did.");
+	}).catch(function(err) {
+		console.log("No it didn't. This happened: ", err)
+	});
+}
