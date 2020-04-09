6e567954f23bd6fd7a0bfaddb586e95cd4a55bbe
mzaini30 <muhzaini30@gmail.com>
2020-04-10 07:14:24 +0800

    Oke.

--- pug/template.js
+++ pug/template.js
@@ -8,7 +8,9 @@ $('.navbar-brand').click(() => {
 
 if ('serviceWorker' in navigator) {
 	console.log("Will the service worker register?");
-	navigator.serviceWorker.register('service-worker.js').then(function(reg){
+	navigator.serviceWorker.register('service-worker.js', {
+        scope: '.' // <--- THIS BIT IS REQUIRED
+    }).then(function(reg){
 		console.log("Yes, it did.");
 	}).catch(function(err) {
 		console.log("No it didn't. This happened: ", err)
