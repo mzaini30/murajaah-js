778f11ec69ce3b184ce1c92350e7b234e08d7d86
mzaini30 <muhzaini30@gmail.com>
2020-04-09 09:23:18 +0800

    upload

--- pug/index.js
+++ pug/index.js
@@ -1,3 +1,23 @@
+var dataHafalan = () => {
+	$.get(database, data => {
+		var datanya = new OlahJson(data)
+		var ambilDataHafalan = datanya.query(`murajaah?id_user=${localStorage.idUserMurajaah}`).get().reverse()
+		var buatTabel = ''
+		for (var x of ambilDataHafalan){
+			buatTabel += `
+				<tr>
+					<td>${x.tanggal}</td>
+					<td>${x.surat}</td>
+					<td>${x.dari}</td>
+					<td>${x.ke}</td>
+				</tr>
+			`
+		}
+		$('.datanya').html(buatTabel)
+	})
+}
+dataHafalan()
+
 if(!localStorage.idUserMurajaah){
 	$('.modalLoginDulu').modal()	
 }
@@ -12,6 +32,7 @@ $('.formLogin').on('submit', x => {
 			localStorage.setItem('idUserMurajaah', cariUsername[0].id)
 			localStorage.setItem('username', cariUsername[0].username)
 			$('.modalLoginDulu').modal('hide')
+			dataHafalan()
 		} else {
 			alert('Akun belum ada. Daftar dulu')
 		}
