7797c22b03872523f5d33b9e555eb41a5bddf0ed
mzaini30 <muhzaini30@gmail.com>
2020-04-10 08:48:13 +0800

    Oke.

--- html/service-worker.js
+++ html/service-worker.js
@@ -16,3 +16,11 @@ self.addEventListener("install", function(installEvent){
     })
   )
 })
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      return response || fetch(event.request);
+    })
+  );
+});
