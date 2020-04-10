8d07afee0ce47b76bf1aa1d702c04d0ddf35451d
mzaini30 <mzaini30@users.noreply.github.com>
2020-04-10 00:50:15 +0000

    deploy: 33f25e4a24dc35af44d04c095d4fdbd9475ec934

--- service-worker.js
+++ service-worker.js
@@ -19,8 +19,13 @@ self.addEventListener("install", function(installEvent){
 
 self.addEventListener('fetch', function(event){
   event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request);
+    caches.match(event.request).then(function(resp){
+      return resp || fetch(event.request).then(function(response){
+        return caches.open(staticFile).then(function(cache){
+          cache.put(event.request, response.clone());
+          return response;
+        });  
+      });
     })
   );
 });
