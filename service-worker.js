86993644ad0f86680032cab18081568c8ba969bc
mzaini30 <mzaini30@users.noreply.github.com>
2020-04-10 00:31:02 +0000

    deploy: fe128c5e797db0ccf54f835c344dcbe336e20641

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,11 @@ const staticFile = "murajaah"
 const assets = [
   "/kelinci.gif",
   "/",
-  "/index.html"
+  "/index.html",
+  "/vendor/bootstrap/css/bootstrap.min.css",
+  "/vendor/jquery.min.js",
+  "/vendor/bootstrap/js/bootstrap.min.js",
+  "/vendor/olahJson.min.js"
 ]
 
 self.addEventListener("install", function(installEvent){
